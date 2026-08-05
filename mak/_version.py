2eea3f3ac239ea8fa53619e73496e614250cd268
chaseungjoon <cktmdwns3@gmail.com>
2026-08-05 15:06:47 +0200

    Update version to 0.5.6b

--- mak/_version.py
+++ mak/_version.py
@@ -11,5 +11,5 @@ keep it in step with ``__version__``.
 
 from __future__ import annotations
 
-__version__ = "0.5.5b0"
-__version_display__ = "0.5.5 Beta"
+__version__ = "0.5.6b0"
+__version_display__ = "0.5.6 Beta"
